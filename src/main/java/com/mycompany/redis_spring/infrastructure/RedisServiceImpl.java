36e68df6c560e9bbc48fe37472cb2b0492bca6c5
Colin But <colinbut@users.noreply.github.com>
2018-01-14 23:01:16 +0000

    adding expiry to cache capabilities

--- src/main/java/com/mycompany/redis_spring/infrastructure/RedisServiceImpl.java
+++ src/main/java/com/mycompany/redis_spring/infrastructure/RedisServiceImpl.java
@@ -9,17 +9,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.TimeUnit;
+
 @Service
 public class RedisServiceImpl implements RedisService {
 
     @Autowired
     private RedisTemplate<String, Object> redisTemplate;
 
+    //region cache capabilities - storing & retrieving key-value pairs
     public Object getValue(final String key) {
         return redisTemplate.opsForValue().get(key);
     }
 
     public void setValue(final String key, final String value) {
         redisTemplate.opsForValue().set(key, value);
+
+        // TTL expiry
+        redisTemplate.expire(key, 3, TimeUnit.MINUTES); // expire after 3 minutes
     }
+    //endregion
+
+
 }
