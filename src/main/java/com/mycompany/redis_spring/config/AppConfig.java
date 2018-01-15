db3ed51eed73b1190fe6361ae4677e6dff61ecb0
Colin But <colinbut@users.noreply.github.com>
2018-01-15 22:59:07 +0000

    added configuration to allow pub-sub using redis

--- src/main/java/com/mycompany/redis_spring/config/AppConfig.java
+++ src/main/java/com/mycompany/redis_spring/config/AppConfig.java
@@ -5,10 +5,14 @@
  */
 package com.mycompany.redis_spring.config;
 
+import com.mycompany.redis_spring.infrastructure.RedisMessageListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -27,6 +31,21 @@ public class AppConfig {
         redisTemplate.setHashValueSerializer(new GenericToStringSerializer<>(Object.class));
         redisTemplate.setValueSerializer(new GenericToStringSerializer<>(Object.class));
         return redisTemplate;
+    }
+
+    @Bean
+    MessageListenerAdapter messageListenerAdapter() {
+        return new MessageListenerAdapter(new RedisMessageListener());
+    }
+
+    @Bean
+    RedisMessageListenerContainer redisMessageListenerContainer() {
+        final RedisMessageListenerContainer redisMessageListenerContainer = new RedisMessageListenerContainer();
+
+        redisMessageListenerContainer.setConnectionFactory(jedisConnectionFactory());
+        redisMessageListenerContainer.addMessageListener(messageListenerAdapter(), new ChannelTopic("my-queue"));
+
+        return redisMessageListenerContainer;
     }
 
 }
---
+++ src/main/java/com/mycompany/redis_spring/infrastructure/RedisMessageListener.java
@@ -0,0 +1,16 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2018 Colin But. All rights reserved.
+ * |-------------------------------------------------
+ */
+package com.mycompany.redis_spring.infrastructure;
+
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+
+public class RedisMessageListener implements MessageListener {
+    @Override
+    public void onMessage(Message message, byte[] bytes) {
+        System.out.println("Received by RedisMessageListener " + message.toString());
+    }
+}
