65a109b2b5d8c1159034fca49fb7971d1840c6e5
Colin But <colinbut@users.noreply.github.com>
2018-01-16 21:41:21 +0000

    refactor example project into a multi-module project to accomodate multiple examples

---
+++ spring-boot-way/src/main/java/com/mycompany/redis_spring/springbootway/SpringBootWayApplication.java
@@ -0,0 +1,12 @@
+package com.mycompany.redis_spring.springbootway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWayApplication.class, args);
+	}
+}
---
+++ spring-boot-way/src/test/java/com/mycompany/redis_spring/springbootway/SpringBootWayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mycompany.redis_spring.springbootway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootWayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
