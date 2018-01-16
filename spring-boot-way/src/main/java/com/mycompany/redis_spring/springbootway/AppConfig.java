651246c96f15662386c7a319223f5dc587a9d975
Colin But <colinbut@users.noreply.github.com>
2018-01-16 22:18:09 +0000

    refactored extracted configuration out to separate config class

---
+++ spring-boot-way/src/main/java/com/mycompany/redis_spring/springbootway/AppConfig.java
@@ -0,0 +1,50 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2018 Colin But. All rights reserved.
+ * |-------------------------------------------------
+ */
+package com.mycompany.redis_spring.springbootway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+
+import java.util.concurrent.CountDownLatch;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    RedisMessageListenerContainer container(RedisConnectionFactory redisConnectionFactory, MessageListenerAdapter messageListenerAdapter) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+
+        container.setConnectionFactory(redisConnectionFactory);
+        container.addMessageListener(messageListenerAdapter, new PatternTopic("chat"));
+
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter messageListenerAdapter(){
+        return new MessageListenerAdapter(receiver(), "receiveMessage");
+    }
+
+    @Bean
+    Receiver receiver() {
+        return new Receiver(countDownLatch());
+    }
+
+    @Bean
+    CountDownLatch countDownLatch(){
+        return new CountDownLatch(1);
+    }
+
+    @Bean
+    StringRedisTemplate template(RedisConnectionFactory redisConnectionFactory) {
+        return new StringRedisTemplate(redisConnectionFactory);
+    }
+}
--- spring-boot-way/src/main/java/com/mycompany/redis_spring/springbootway/SpringBootWayApplication.java
+++ spring-boot-way/src/main/java/com/mycompany/redis_spring/springbootway/SpringBootWayApplication.java
@@ -4,49 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.listener.PatternTopic;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
-
-import java.util.concurrent.CountDownLatch;
 
 @SpringBootApplication
 public class SpringBootWayApplication {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SpringBootWayApplication.class);
-
-    @Bean
-    RedisMessageListenerContainer container(RedisConnectionFactory redisConnectionFactory, MessageListenerAdapter messageListenerAdapter) {
-        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-
-        container.setConnectionFactory(redisConnectionFactory);
-        container.addMessageListener(messageListenerAdapter, new PatternTopic("chat"));
-
-        return container;
-    }
-
-    @Bean
-    MessageListenerAdapter messageListenerAdapter(){
-        return new MessageListenerAdapter(receiver(), "receiveMessage");
-    }
-
-    @Bean
-    Receiver receiver() {
-        return new Receiver(countDownLatch());
-    }
-
-    @Bean
-    CountDownLatch countDownLatch(){
-        return new CountDownLatch(1);
-    }
-
-    @Bean
-    StringRedisTemplate template(RedisConnectionFactory redisConnectionFactory) {
-        return new StringRedisTemplate(redisConnectionFactory);
-    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWayApplication.class, args);
